d905a92e340d1fd4544188d8dcfa4d0582e2da95
Dassyareg <dassyareg@gmail.com>
2022-09-10 13:55:05 +0100

    Added the variables to run basic simulation

--- main.go
+++ main.go
@@ -1,32 +1,55 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+// constant values
+const (
+	// The screen Width and Screen Height for our simualtion
+	screenWidth, screenHeight = 640, 360 
+)
+
+var pix = color.RGBA{249, 105, 14, 255}
 // Game implements ebiten.Game interface.
 type Game struct{}
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
-    // Write your game's logical update.
-    return nil
+	// Write your game's logical update.
+	return nil
 }
 
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
-func (g *Game) Draw(screen *ebiten.Image) {
-    // Write your game's rendering.
+func (g *Game) Draw(screen *ebiten.Image, boid *boids) {
+	// Write your game's rendering.
+
+	for _, v := range boid {
+		screen.Set(int(v.x + 1), int(v.y), pix)
+		screen.Set(int(v.x - 1), int(v.y), pix)
+		screen.Set(int(v.x), int(v.y + 1), pix)
+		screen.Set(int(v.x), int(v.y - 1), pix)
+
+	}
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return 320, 240
+	return outsideWidth, outsideHeight
 }
 
 func main() {
-    game := &Game{}
-    // Specify the window size as you like. Here, a doubled size is specified.
-    ebiten.SetWindowSize(640, 480)
-    ebiten.SetWindowTitle("Your game's title")
-    // Call ebiten.RunGame to start your game loop.
-    if err := ebiten.RunGame(game); err != nil {
-        log.Fatal(err)
-    }
+	game := &Game{}
+	// Specify the window size as you like. Here, a doubled size is specified.
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Go Go Boids!!!")
+	// Call ebiten.RunGame to start your game loop.
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
